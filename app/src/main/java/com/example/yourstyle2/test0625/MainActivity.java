88665d26df3943867a37186026fea4460c6a2850
meg0401 <megumi_add@yahoo.co.jp>
2017-06-25 10:35:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yourstyle2/test0625/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yourstyle2.test0625;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
